05dbb87ca4a0cad91e46d6019997935bc52d5909
신종욱 <71578653+beyondthemist@users.noreply.github.com>
2024-07-03 01:46:47 +0900

    불필요한 Microservice 삭제

--- backend/HtpMS/src/main/java/com/a303/htpms/domain/htp/service/HtpService.java
+++
@@ -1,5 +0,0 @@
-package com.a303.htpms.domain.htp.service;
-
-public interface HtpService {
-    String htpTestAnalysis();
-}
--- backend/HtpMS/src/main/java/com/a303/htpms/global/config/jpa/JpaRepositoryConfig.java
+++
@@ -1,7 +0,0 @@
-package com.a303.htpms.global.config.jpa;
-
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-public class JpaRepositoryConfig {
-}
